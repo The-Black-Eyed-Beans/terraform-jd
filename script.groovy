92c25adb49a52364b313da510d6893d1614c8b77
joshua-davis1 <joshua.davis@smoothstack.com>
2022-03-08 22:02:01 -0500

    refactored code, renamed tfvars file

--- script.groovy
+++ script.groovy
@@ -1,13 +1,13 @@
 def planAndApply() {
-    if (params.IS_DESTROYING == false) {
+    if (!params.IS_DESTROYING) {
         sh "terraform init -input=false"
-        sh "terraform plan -var-file=input.tfvars -out=tfplan -input=false && terraform apply tfplan"
+        sh "terraform plan -out=tfplan -input=false && terraform apply tfplan"
     }
 }
 
 def postAlways() {
     if (params.IS_DESTROYING) {
-        sh "terraform destroy -auto-approve -var-file=input.tfvars"
+        sh "terraform destroy -auto-approve"
     }
 }
 
