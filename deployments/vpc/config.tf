0fc9002fade44aead46beb0f86c647181038b5e4
joshua-davis1 <joshua.davis@smoothstack.com>
2022-03-26 11:50:17 -0400

    BA-144 config

--- deployments/vpc/config.tf
+++ deployments/vpc/config.tf
@@ -11,6 +11,8 @@ terraform {
 
 provider "aws" {
     region                   = var.AWS_REGION
+    shared_config_files      = ["$HOME/.aws/config"]
+    shared_credentials_files = ["$HOME/.aws/credentials"]
 }
 
 terraform {
