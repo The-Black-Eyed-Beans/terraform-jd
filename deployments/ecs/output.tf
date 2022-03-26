d9ca91a065f4bc1dc3bd5b736d84c02ded2dab03
joshua-davis1 <joshua.davis@smoothstack.com>
2022-03-26 17:02:49 -0400

    BA-144 hid all output

--- deployments/ecs/output.tf
+++ deployments/ecs/output.tf
@@ -1,47 +1,47 @@
 output vpc_id {
   value       = module.vpc["vpc-1"].get_vpc.vpc_id
-  sensitive   = false
+  sensitive   = true
   description = "VPC iD"
 }
 
 output private_subnets {
   value       = module.vpc["vpc-1"].get_vpc.private_subnets
-  sensitive = false
+  sensitive = true
   description = "List of private subnet IDs"
 }
 
 output security_groups {
   value       = [for sg in module.my_security_group: {name: sg.get_sg.name, id: sg.get_sg.id}]
-  sensitive   = false
+  sensitive   = true
   description = "List of Security Group IDs"
 }
 
 output load_balancer {
   value       = module.my_load_balancer.get_lbs[0].name
-  sensitive   = false
+  sensitive   = true
   description = "List of Load Balancers"
 }
 
 output cluster {
   value = aws_ecs_cluster.cluster.name
-  sensitive   = false
+  sensitive   = true
   description = "Name of create ECS Cluster"
 }
 
 output ssl_cert {
   value = aws_acm_certificate.cert.arn
-  sensitive   = false
+  sensitive   = true
   description = "SSL of Gateway domain."
 }
 
 output service_secrets {
   value = module.secrets.service_secrets
-  sensitive   = false
+  sensitive   = true
   description = "List of service secrets."
 }
 
 output proxy_service_secrets {
   value = module.secrets.proxy_secret_name
-  sensitive   = false
+  sensitive   = true
   description = "Secret for proxy server."
 }
