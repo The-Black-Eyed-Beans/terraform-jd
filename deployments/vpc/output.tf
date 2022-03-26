9d0ec075774ce27239b905dd5d27e8889befc18c
joshua-davis1 <joshua.davis@smoothstack.com>
2022-03-26 17:27:48 -0400

    BA-144 modularized ecs deployment

---
+++ deployments/vpc/output.tf
@@ -0,0 +1,11 @@
+output vpc_id {
+  value       = module.vpc["vpc-1"].get_vpc.vpc_id
+  sensitive   = true
+  description = "VPC iD"
+}
+
+output private_subnets {
+  value       = module.vpc["vpc-1"].get_vpc.private_subnets
+  sensitive = true
+  description = "List of private subnet IDs"
+}
