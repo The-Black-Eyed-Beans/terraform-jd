a159d66245ed728efb67e7f82a21079de3434a7a
joshua-davis1 <joshua.davis@smoothstack.com>
2022-03-27 22:05:11 -0400

    BA-144 added destroy condition

--- script.groovy
+++ script.groovy
@@ -5,26 +5,26 @@ def init() {
         terraform init \
             -no-color \
             -backend-config="bucket=${BUCKET}" \
-            -backend-config="key=${BUCKET_KEY}" \
+            -backend-config='key=$BUCKET_KEY' \
             -backend-config="region=${REGION}" \
             -backend-config='profile=$AWS_PROFILE'
     """
 }
 
 def plan() {
-    if (!params.IS_DESTROYING) {
+    if (!env.IS_DESTROYING.toBoolean()) {
         sh "terraform plan -out=tfplan"
     }
 }
 
 def apply() {
-    if (!params.IS_DESTROYING) {
+    if (!env.IS_DESTROYING.toBoolean()) {
         sh "terraform apply tfplan"
     }
 }
 
 def postAlways() {
-    if (params.IS_DESTROYING) {
+    if (env.IS_DESTROYING.toBoolean()) {
         sh "terraform destroy -auto-approve"
     }
 }
@@ -33,8 +33,8 @@ def postCleanup() {
     sh "rm -rf ./*"
 }
 
-def postSuccess(CLUSTER_TYPE) {
-    if (!params.IS_DESTROYING) {
+def postSuccess() {
+    if (!env.IS_DESTROYING.toBoolean()) {
         if (CLUSTER_TYPE == "ECS") {
             sh 'curl -u BuildUser:$BUILD_USER_TOKEN -i "https://jenkins.theblackeyedbeans.net/job/aline-jd/job/Infrastructure as Code/job/ecs/build?token=$ECS_PIPELINE_TOKEN" > /dev/null'
         } else {sh 'curl -u BuildUser:$BUILD_USER_TOKEN -i "https://jenkins.theblackeyedbeans.net/job/aline-jd/job/Infrastructure as Code/job/eks/build?token=$EKS_PIPELINE_TOKEN" > /dev/null'}
